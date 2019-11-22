675ced54ccaf96ab14be61a236631cfe7399a6a4
Kimi_yu <1067414607@qq.com>
2019-11-22 18:21:02 +0800

    Alpha最终版本

--- 031702441_余廷龙/teamwork.py
+++ 031702441_余廷龙/teamwork.py
@@ -3,32 +3,32 @@
 
 import json
 import MySQLdb
+import time
+import datetime
 
 
 def issue_order():
     json_data = {
         "error_code": 0,
         "data": {
-            "canteen": "sss",
-            "delivery_time": "null",
-            "money": 1.0,
-            "order_delivery_time": "null",
-            "order_number": "10023278",
-            "order_status": "0",
+            "order_number": "1",
             "order_type": "0",
-            "requirement_customer": "ada",
-            "requirement_delivery": "fasd",
-            "result_time": "12:10",
-            "scheduled_delivery_time": "12:00",
+            "order_status": "0",
+            "canteen": "紫荆园二楼",
             "s_dormitory_1": "32",
             "s_dormitory_2": "316",
-            "telephone_delivery": "13223332333",
-            "telephone_customer": "13223332333"
+            "money": "1.0",
+            "requirement_customer": "水煮肉片，不加香菜",
+            "requirement_delivery": "走路，可能会有点慢",
+            "order_delivery_time": "null",
+            "scheduled_delivery_countdown": 20,
+            "result_countdown": 10,
+            "telephone_delivery": "13205030028",
+            "telephone_customer": "13859040643"
         }
     }
 
     canteen = json_data["data"]["canteen"]
-    delivery_time = json_data["data"]["delivery_time"]
     money = json_data["data"]["money"]
     order_delivery_time = json_data["data"]["order_delivery_time"]
     order_number = json_data["data"]["order_number"]
@@ -36,26 +36,31 @@ def issue_order():
     order_type = json_data["data"]["order_type"]
     requirement_customer = json_data["data"]["requirement_customer"]
     requirement_delivery = json_data["data"]["requirement_delivery"]
-    result_time = json_data["data"]["result_time"]
-    scheduled_delivery_time = json_data["data"]["scheduled_delivery_time"]
+    result_time = (
+            datetime.datetime.now() + datetime.timedelta(minutes=json_data["data"]["result_countdown"])).strftime(
+        "%H:%M")  # 自动取消时间
+    scheduled_delivery_time = (datetime.datetime.now() + datetime.timedelta(
+        minutes=json_data["data"]["scheduled_delivery_countdown"])).strftime(
+        "%H:%M")  # 预计送达时间
     s_dormitory_1 = json_data["data"]["s_dormitory_1"]
     s_dormitory_2 = json_data["data"]["s_dormitory_2"]
     telephone_delivery = json_data["data"]["telephone_delivery"]
     telephone_customer = json_data["data"]["telephone_customer"]
-
+    print (scheduled_delivery_time)
+    print (result_time)
     db = MySQLdb.connect("47.101.217.136", "phd", "11111111", "software", charset='utf8')
     cursor = db.cursor()
     sql = """INSERT INTO order2 (order_number, order_type, order_status, canteen,s_dormitory_1, 
             s_dormitory_2, money, requirement_customer, requirement_delivery, 
-            order_delivery_time, scheduled_delivery_time, delivery_time, result_time, telephone_delivery, 
+            order_delivery_time, scheduled_delivery_time, result_time, telephone_delivery, 
             telephone_customer)
-          values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+          values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
 
     try:
         # 执行sql语句
         cursor.execute(sql, (order_number, order_type, order_status, canteen, s_dormitory_1,
                              s_dormitory_2, money, requirement_customer, requirement_delivery,
-                             order_delivery_time, scheduled_delivery_time, delivery_time,
+                             order_delivery_time, scheduled_delivery_time,
                              result_time, telephone_delivery, telephone_customer))
         # 提交到数据库执行
         db.commit()
@@ -80,33 +85,52 @@ def get_order():
     json_data = {
         "error_code": 0,
         "data": {
-            "order_number": "13124329",
+            "order_number": "1",
+            "order_type": "0",
+            "order_status": "0",
+            "canteen": "紫荆园二楼",
+            "s_dormitory_1": "32",
             "s_dormitory_2": "316",
-            "requirement_customer": "dfs",
-            "requirement_delivery": "asd",
-            "scheduled_delivery_time": "12:00",
-            "delivery_time": "1234",
+            "money": "2.0",
+            "requirement_customer": "水煮肉片，不加香菜",
+            "requirement_delivery": "走路，可能会有点慢",
+            #"order_delivery_time": "null", 删掉
+            "telephone_delivery": "13205030028",
+            "telephone_customer": "13859040643"
         }
     }
+
+    canteen = json_data["data"]["canteen"]
+    money = json_data["data"]["money"]
+    order_delivery_time = datetime.datetime.now().strftime("%H:%M")
     order_number = json_data["data"]["order_number"]
-    s_dormitory_2 = json_data["data"]["s_dormitory_2"]
+    order_status = json_data["data"]["order_status"]
+    order_type = json_data["data"]["order_type"]
     requirement_customer = json_data["data"]["requirement_customer"]
     requirement_delivery = json_data["data"]["requirement_delivery"]
-    scheduled_delivery_time = json_data["data"]["scheduled_delivery_time"]
-    delivery_time = json_data["data"]["delivery_time"]
+    s_dormitory_1 = json_data["data"]["s_dormitory_1"]
+    s_dormitory_2 = json_data["data"]["s_dormitory_2"]
+    telephone_delivery = json_data["data"]["telephone_delivery"]
+    telephone_customer = json_data["data"]["telephone_customer"]
 
     db = MySQLdb.connect("47.101.217.136", "phd", "11111111", "software", charset='utf8')
     cursor = db.cursor()
-    sql = """INSERT INTO order2 (order_number, s_dormitory_2, requirement_customer, requirement_delivery,
-                            scheduled_delivery_time, delivery_time)
-                        values(%s,%s,%s,%s,%s,%s)"""
+    sql = """update order2 
+                set order_type= %s, order_status= %s, canteen= %s,s_dormitory_1= %s, 
+                s_dormitory_2= %s, money= %s, requirement_customer= %s, requirement_delivery= %s, 
+                order_delivery_time= %s, telephone_delivery= %s, 
+                telephone_customer= %s
+                where order_number= %s"""
+
     try:
-        cursor.execute(sql, (order_number, s_dormitory_2, requirement_customer, requirement_delivery,
-        scheduled_delivery_time, delivery_time))
+        # 执行sql语句
+        cursor.execute(sql, (order_type, order_status, canteen, s_dormitory_1,
+                             s_dormitory_2, money, requirement_customer, requirement_delivery,
+                             order_delivery_time, telephone_delivery, telephone_customer, order_number))
         db.commit()
     except:
         db.rollback()
-        print "error"
+        print ("error")
 
     db.close()
     res = {
@@ -123,7 +147,7 @@ def show_historical_business_orders():
     json_data = {
         "error_code": 0,
         "data": {
-            "telephone": "13223332333",
+            "telephone": "13223332333"
         }
     }
     res = {
@@ -131,8 +155,18 @@ def show_historical_business_orders():
         "data": []
     }
     telephone = json_data["data"]["telephone"]
+    time_now = time.strftime('%H:%M', time.localtime(time.time()))
     db = MySQLdb.connect("47.101.217.136", "phd", "11111111", "software", charset='utf8')
     cursor = db.cursor(MySQLdb.cursors.DictCursor)
+    sql = """update order2
+            set order_status='3'
+            where telephone_delivery= %s and result_time< %s """
+    try:
+        cursor.execute(sql, (telephone, time_now))
+        db.commit()
+    except:
+        print("Error: unable to update data")
+
     sql = """select * 
             from order2 
             where telephone_delivery= %s """
@@ -145,7 +179,6 @@ def show_historical_business_orders():
         print("Error: unable to fecth data")
 
     db.close()
-    print (res)
     return json.dumps(res, ensure_ascii=False)
 
 
@@ -154,7 +187,7 @@ def show_historical_customer_orders():
     json_data = {
         "error_code": 0,
         "data": {
-            "telephone": "13223332333",
+            "telephone": "13223332333"
         }
     }
     res = {
@@ -162,8 +195,18 @@ def show_historical_customer_orders():
         "data": []
     }
     telephone = json_data["data"]["telephone"]
+    time_now = time.strftime('%H:%M', time.localtime(time.time()))
     db = MySQLdb.connect("47.101.217.136", "phd", "11111111", "software", charset='utf8')
     cursor = db.cursor(MySQLdb.cursors.DictCursor)
+    sql = """update order2
+                set order_status='3'
+                where telephone_customer= %s and result_time< %s """
+    try:
+        cursor.execute(sql, (telephone, time_now))
+        db.commit()
+    except:
+        print("Error: unable to update data")
+
     sql = """select * 
             from order2 
             where telephone_customer= %s """
@@ -176,5 +219,7 @@ def show_historical_customer_orders():
         print("Error: unable to fecth data")
 
     db.close()
-    print (res)
     return json.dumps(res, ensure_ascii=False)
+
+
+get_order()

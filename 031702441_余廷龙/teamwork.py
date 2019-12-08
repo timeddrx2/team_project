fea29e95c718b2218a3ab1df5abdf664f5cada93
Kimi_yu <1067414607@qq.com>
2019-12-08 18:50:07 +0800

    搜索功能的实现

--- 031702441_余廷龙/teamwork.py
+++ 031702441_余廷龙/teamwork.py
@@ -31,7 +31,7 @@ def issue_order():
     canteen = json_data["data"]["canteen"]
     money = json_data["data"]["money"]
     order_delivery_time = json_data["data"]["order_delivery_time"]
-    order_number = json_data["data"]["order_number"]
+    order_number = str(int(time.time() * 10))
     order_status = json_data["data"]["order_status"]
     order_type = json_data["data"]["order_type"]
     requirement_customer = json_data["data"]["requirement_customer"]
@@ -48,7 +48,7 @@ def issue_order():
     telephone_customer = json_data["data"]["telephone_customer"]
     print (scheduled_delivery_time)
     print (result_time)
-    db = MySQLdb.connect("88888", "888", "88888", "software", charset='utf8')
+    db = MySQLdb.connect("47.101.217.136", "phd", "11111111", "software", charset='utf8')
     cursor = db.cursor()
     sql = """INSERT INTO order2 (order_number, order_type, order_status, canteen,s_dormitory_1, 
             s_dormitory_2, money, requirement_customer, requirement_delivery, 
@@ -94,7 +94,7 @@ def get_order():
             "money": "2.0",
             "requirement_customer": "水煮肉片，不加香菜",
             "requirement_delivery": "走路，可能会有点慢",
-            #"order_delivery_time": "null", 删掉
+            # "order_delivery_time": "null", 删掉
             "telephone_delivery": "13205030028",
             "telephone_customer": "13859040643"
         }
@@ -113,7 +113,7 @@ def get_order():
     telephone_delivery = json_data["data"]["telephone_delivery"]
     telephone_customer = json_data["data"]["telephone_customer"]
 
-    db = MySQLdb.connect("88888", "888", "88888", "software", charset='utf8')
+    db = MySQLdb.connect("47.101.217.136", "phd", "11111111", "software", charset='utf8')
     cursor = db.cursor()
     sql = """update order2 
                 set order_type= %s, order_status= %s, canteen= %s,s_dormitory_1= %s, 
@@ -147,7 +147,7 @@ def show_historical_business_orders():
     json_data = {
         "error_code": 0,
         "data": {
-            "telephone": "13223332333"
+            "telephone": "13205030028"
         }
     }
     res = {
@@ -156,7 +156,7 @@ def show_historical_business_orders():
     }
     telephone = json_data["data"]["telephone"]
     time_now = time.strftime('%H:%M', time.localtime(time.time()))
-    db = MySQLdb.connect("88888", "888", "88888", "software", charset='utf8')
+    db = MySQLdb.connect("47.101.217.136", "phd", "11111111", "software", charset='utf8')
     cursor = db.cursor(MySQLdb.cursors.DictCursor)
     sql = """update order2
             set order_status='3'
@@ -169,7 +169,8 @@ def show_historical_business_orders():
 
     sql = """select * 
             from order2 
-            where telephone_delivery= %s """
+            where telephone_delivery= %s
+            order by order_number desc"""
     try:
         cursor.execute(sql % telephone)
         results = cursor.fetchall()
@@ -187,7 +188,7 @@ def show_historical_customer_orders():
     json_data = {
         "error_code": 0,
         "data": {
-            "telephone": "13223332333"
+            "telephone": "13205030028"
         }
     }
     res = {
@@ -196,7 +197,7 @@ def show_historical_customer_orders():
     }
     telephone = json_data["data"]["telephone"]
     time_now = time.strftime('%H:%M', time.localtime(time.time()))
-    db = MySQLdb.connect("88888", "888", "88888", "software", charset='utf8')
+    db = MySQLdb.connect("47.101.217.136", "phd", "11111111", "software", charset='utf8')
     cursor = db.cursor(MySQLdb.cursors.DictCursor)
     sql = """update order2
                 set order_status='3'
@@ -209,7 +210,8 @@ def show_historical_customer_orders():
 
     sql = """select * 
             from order2 
-            where telephone_customer= %s """
+            where telephone_customer= %s 
+            order by order_number desc"""
     try:
         cursor.execute(sql % telephone)
         results = cursor.fetchall()
@@ -222,4 +224,90 @@ def show_historical_customer_orders():
     return json.dumps(res, ensure_ascii=False)
 
 
-get_order()
+# 筛选点单广场
+def sift_customer_order():
+    json_data = {
+        "error_code": 0,
+        "data": {
+            "s_dormitory_1": "32",
+            "canteen": "紫荆园一楼",
+        }
+    }
+    res = {
+        "error_code": 0,
+        "data": []
+    }
+    time_now = time.strftime('%H:%M', time.localtime(time.time()))
+    s_dormitory_1 = json_data["data"]["s_dormitory_1"]
+    canteen = json_data["data"]["canteen"]
+    db = MySQLdb.connect("47.101.217.136", "phd", "11111111", "software", charset='utf8')
+    cursor = db.cursor(MySQLdb.cursors.DictCursor)
+
+    sql = """update order2
+                set order_status='3'
+                where result_time< '%s' """
+    try:
+        cursor.execute(sql % time_now)
+        db.commit()
+    except:
+        print("update error")
+
+    sql = """select * 
+                from order2 
+                where canteen= %s and  s_dormitory_1= %s and order_type='1' and order_status='0'
+                order by order_number desc"""
+    try:
+        cursor.execute(sql, (canteen, s_dormitory_1))
+        results = cursor.fetchall()
+        for row in results:
+            res["data"].append(row)
+    except:
+        print("Error: unable to fecth data")
+
+    db.close()
+    return json.dumps(res, ensure_ascii=False)
+
+
+# 筛选配送广场
+def sift_delivery_order():
+    json_data = {
+        "error_code": 0,
+        "data": {
+            "s_dormitory_1": "32",
+            "canteen": "紫荆园一楼",
+        }
+    }
+    res = {
+        "error_code": 0,
+        "data": []
+    }
+    time_now = time.strftime('%H:%M', time.localtime(time.time()))
+    s_dormitory_1 = json_data["data"]["s_dormitory_1"]
+    canteen = json_data["data"]["canteen"]
+    db = MySQLdb.connect("47.101.217.136", "phd", "11111111", "software", charset='utf8')
+    cursor = db.cursor(MySQLdb.cursors.DictCursor)
+
+    sql = """update order2
+                set order_status='3'
+                where result_time< '%s' """
+    try:
+        cursor.execute(sql % time_now)
+        db.commit()
+    except:
+        print("update error")
+
+    sql = """select * 
+                from order2 
+                where canteen= %s and  s_dormitory_1= %s and order_type='0' and order_status='0'
+                order by order_number desc"""
+    try:
+        cursor.execute(sql, (canteen, s_dormitory_1))
+        results = cursor.fetchall()
+        for row in results:
+            res["data"].append(row)
+    except:
+        print("Error: unable to fecth data")
+
+    db.close()
+    return json.dumps(res, ensure_ascii=False)
+sift_delivery_order()
